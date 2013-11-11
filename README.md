58623a09c27501656963403deccf8352f2c3ddf2
Daniel Williams <dwilliams@port8080.net>
2013-11-11 16:49:27 -0700

    Adding a bit of code and some description to the readme.
    ~Daniel Williams <dwilliams@port8080.net>

--- README.md
+++ README.md
@@ -2,3 +2,58 @@ dpyfs
 =====
 
 A python based network distributed file system project.
+
+This is starting off as a "because I can" or a "because I want to learn more
+about it" type of project.  My goals are going to be as follows:
+
+ * Simple: Simplicity is the key to making things useable and maintainable.
+ * Redundant: What's the use in storing anything if it's going to get wiped out?
+ * Embedded (not quite, but close): I want to run this on small, power efficent
+   ARM processors or Atom processors (ones that will run OpenBSD).
+
+I'm looking to trade speed (because I don't really need much of it) for
+simplicity and redundancy.  I've lost too many files to complicated systems
+that, while providing redundancy, were too complicated to maintain or recover
+from some error.  Thus, with a little boredom, this project was born.  The
+system will draw a bit from filesystems like Ceph or XtreemFS, but won't
+actually copy them.  While this will help with the licensing, in reality I'm
+just to lazy to try to decrypt their source.
+
+dpyfs (pronounced dippy-F-S) will feature three main components: the storage
+daemon, the director daemon, and the client.  There will probably end up being
+more than one client, but for now I'll focus on something FUSE-ish.
+
+Storage Daemon
+--------------
+The initial plan for the storage daemon is to be a simple key value store.  I
+know, I know, I can just use MongoDB or something, but I want less horsepower
+and simpler features.  I plan to use a file based backing store with some basic
+algorithms to glean information from the system the daemon is running on.  The
+information will include things like system load, disk usage, file numbers, etc.
+The storage daemon should also have some basic integrity checking functionality.
+The storage daemon will be accessed via HTTP initially, and updated to use HTTPS
+with some sort of certificate authority structure in the future (maybe leverage
+Puppet for handling this?).  Any file chunk that is to be stored will use hashes
+for it's key.  I'm going to enforce the storage daemons to store chunks, not
+files.  While this does slightly increase complexity, it allows for added
+benefits like deduplication and internal integrity checking more easily.
+
+Director Daemon
+---------------
+The director daemon will provide the main interaction point with the cluster.
+Any client that wishes to access a file will talk to a director daemon.  While
+this isn't the most performant solution available, it will provide a small
+number of (redundant) points to handle control and integrity of the cluster.
+The plan is to have the director be a file backed key value store that
+translates the file path into a list of chunks.  The director will either gather
+or disperse the chunks based on the file operation.  The director will be a
+proxy for all file accesses to the cluster.  This will hopefully allow features
+like read ahead to be added in the near future.  It also lets the clients be
+much lighter weight as the client doesn't have to deal with load the chunks,
+checking them, and reassembling them.
+
+Client
+------
+Will initially be a ReSTful client similar to Amazon S3 storage.  Will make a
+FUSE-ish file system in the future for setting up Windows shares and UPnP media
+servers.
--- storaged.py
+++ storaged.py
@@ -5,16 +5,24 @@
 # Daniel Williams <dwilliams@port8080.net>
 
 ### IMPORTS ####################################################################
+import logging
 import argparse
 import ConfigParser
 import hashlib
 import web
 
 ### GLOBALS ####################################################################
-config = ConfigParser.ConfigParser()
-configSection = ''
 
 ### FUNCTIONS ##################################################################
+# View function for the good ol' 404
+def notFound():
+    return web.notfound("404 Not Found\n"
+                        "Somethin's amiss.  Might want to check them URLs again.")
+
+# View function for a little internal error love
+def internalError():
+    return web.internalerror("500 Internal Server Error\n"
+                             "I guess I've lost my tools in my hands again.")
 
 ### CLASSES ####################################################################
 # Controller class for redirecting the root to another place
@@ -31,46 +39,79 @@ class info:
 class data:
     def GET(self, hashMD5, hashSHA1):
         return "No data here currently."
-    
+
     def PUT(self, hashMD5, hashSHA1):
         return "Not yet implemented.  Coming soon!"
-    
+
     def DELETE(self, hashMD5, hashSHA1):
         return "Not yet implemented.  Coming soon!"
-    
+
     # Do I need to implement this ass an error, or just leave it out?
     def POST(self, hashMD5, hashSHA1):
         return "Not yet implemented.  May not implement in the future."
 
+# Config handling class.  This should never be accessed by the clients directly.
+class dpyfsConfig:
+    configFile = '/etc/dpyfs/storaged.conf'
+    configSection = 'storage'
+    ipaddr = '0.0.0.0'
+    port = 8080
+    storageDir = '/var/dpyfs/data'
+
+    # Initialize the configuration class
+    def __init__(self, conFile = None):
+        if conFile is not None:
+            self.configFile = conFile
+        # Load the config from the file
+        self.tmpConfig = ConfigParser.ConfigParser()
+        self.tmpConfig.read(self.configFile)
+
+    # Load the config from the proper section
+    def load(self, section = None):
+        if section is not None:
+            self.configSection = section
+        # Read the config options from the section. Need a try catch with better
+        # error handling here.
+        for key, value in self.tmpConfig.items(self.configSection):
+            setattr(self, key, value)
+
+    # Create an IP address for the webserver to use
+    def getIP(self):
+        return web.net.validip("%s:%d" % (self.ipaddr, self.port))
+
 ### MAIN #######################################################################
 def main():
+    # Turn on verbose logging.  I'll make this config driven at a future date.
+    logging.basicConfig(level=logging.DEBUG)
+
     # Parse the command line arguments.
     argparser = argparse.ArgumentParser(description = "The storage daemon for the dpyfs project.")
     argparser.add_argument('--configFile', help = "Configuration file to read from")
     argparser.add_argument('--configSection', help = "Section in the configuration file to use")
     args = argparser.parse_args()
-    
+    logging.debug('Argument datastructure: \n%s' % (str(args)))
+
     # Parse the config file.
-    if 'configFile' in args:
-        config.read(args.configFile)
+    if args.configFile is not None:
+        config = dpyfsConfig(args.configFile)
     else:
-        config.read('/etc/dpyfs/storaged.conf')
-    
-    if 'configSection' in args:
-        configSection = args.configSection
+        config = dpyfsConfig()
+
+    if args.configSection is not None:
+        config.load(args.configSection)
     else:
-        configSection = 'storage'
-    
-    ## Setup the configuration class here.
-    
+        config.load()
+
     # Setup the webserver for handling requests.
     urls = ('/', 'index',
             '/data/([0-9,a-f,A-F]+)/([0-9,a-f,A-F]+)', 'data',
             '/info', 'info')
-    
+
     app = web.application(urls, globals())
+    app.notfound = notFound
+    app.internalerror = internalError
     # Should update this to use web.net.validip()
-    web.httpserver.runsimple(app.wsgifunc(), ("0.0.0.0", config.getint(configSection, 'port')))
+    web.httpserver.runsimple(app.wsgifunc(), config.getIP())
 
 if __name__ == "__main__":
     main()
