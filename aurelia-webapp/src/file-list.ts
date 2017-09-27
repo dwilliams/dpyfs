456c57c8603bb396cd79a6f1964ad90771c3ec62
Daniel Williams <dwilliams@inovonics.com>
2017-09-27 13:24:50 -0600

    Changing contacts to file for the demo.

---
+++ aurelia-webapp/src/file-list.ts
@@ -0,0 +1,45 @@
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+
+// @inject(WebAPI, EventAggregator)
+// export class ContactList {
+  // contacts;
+  // selectedId = 0;
+// 
+  // constructor(private api: WebAPI, ea: EventAggregator) {
+    // ea.subscribe(ContactViewed, msg => this.select(msg.contact));
+    // ea.subscribe(ContactUpdated, msg => {
+      // let id = msg.contact.id;
+      // let found = this.contacts.find(x => x.id == id);
+      // Object.assign(found, msg.contact);
+    // });
+  // }
+// 
+  // created() {
+    // this.api.getContactList().then(contacts => this.contacts = contacts);
+  // }
+// 
+  // select(contact) {
+    // this.selectedId = contact.id;
+    // return true;
+  // }
+// }
+
+
+@inject(HttpClient)
+export class FileList {
+
+    constructor(http) {
+        this.http = http;
+    }
+
+    getSomeJson() {
+        this.http.fetch('something')
+            .then(response => response.json())
+            .then(data => {
+                    console.log(data);
+            })
+    }
+
+}
